588504184a5a24685d17f78eb6ece5ddecf425a9
Tariq Sharayah <Tariq.Sharayah@omicron-jo.com>
2025-09-28 23:13:22 +0300

    end of section 7

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -3,60 +3,21 @@
  useState: lets you create state variables in a functional component.
  useEffect: lets you run side effects (e.g., API calls, subscriptions) when the component renders.
  */
-import { useState } from 'react'
-import { Box, Container, CssBaseline, Typography } from '@mui/material';
+import { Box, Container, CssBaseline } from '@mui/material';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/Dashboard/ActivityDashboard';
-import { useActivities } from "../../lib/hooks/useActivities";
+import { Outlet } from 'react-router';
 
- export default function App() {
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const {activities, isPending} = useActivities();
-
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities!.find(x => x.id === id));
-  }
-
-  const handleOpenForm = (id?: string) => {
-    if (id) handleSelectActivity(id);
-    else handleCancelSelectedActivity();
-    setEditMode(true);
-  }
-
-  const handleFormClose = () => {
-    setEditMode(false);
-  }
-
-  const handleCancelSelectedActivity = () => {
-    setSelectedActivity(undefined);
-  }
+export default function App() {
 
 
 
   return (
     <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
       <CssBaseline />
-      <NavBar openForm={handleOpenForm} />
+      <NavBar />
       <Container maxWidth='xl' sx={{ mt: 3 }}>
-        {!activities || isPending ? (
-          <Typography>Loading ...</Typography>
-        ) : (
-          <ActivityDashboard
-            activities={activities}
-            selectActivity={handleSelectActivity}
-            cancelSelectActivity={handleCancelSelectedActivity}
-            selectedActivity={selectedActivity}
-            editMode={editMode}
-            openForm={handleOpenForm}
-            closeForm={handleFormClose}
-          />
-        )}
-
+        <Outlet />
       </Container>
-
-
     </Box>
   )
 }
